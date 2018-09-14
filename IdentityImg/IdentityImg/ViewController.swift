26bbafdf0b0937878a7e185a06f263ae0cc9f2f6
mefilt <mefilt@gmail.com>
2018-09-14 04:37:54 +0300

    Begin Identiti

---
+++ IdentityImg/IdentityImg/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IdentityImg
+//
+//  Created by Prokofev Ruslan on 14/09/2018.
+//  Copyright © 2018 Waves. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    @IBOutlet var imageViews: [UIImageView]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
